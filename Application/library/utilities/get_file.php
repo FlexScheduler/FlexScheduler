09b3cbab78a6c0a1eafb2c2595e06931bc7964b5
Justin Wolfe <44961417+j-r-wolfe@users.noreply.github.com>
2020-03-09 22:40:43 -0500

    File structure updated
    other minor changes:
    -import feature has improved error handling
    -home page now links to a temporary login to add functionality to the import feature page
    
    bugs:
    -database information (other than admin login data) is hard-coded as host="localhost", databaseName="senprojtest"

---
+++ Application/library/utilities/get_file.php
@@ -0,0 +1,22 @@
+<?php
+function get_file($fileName) {
+    // get file information
+    $targetDir = "uploads/";
+    $targetFilePath = $targetDir . $fileName;
+    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+    // check file is good
+    if ($fileType != "csv") {
+        $exceptionMessage = "File type error: chosen file must be in " .
+            "CSV format.";
+        throw new Exception($exceptionMessage);
+    }
+    if (!move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+        $exceptionMessage = "File upload error: file could not be uploaded " .
+            "to the server.";
+        throw new Exception($exceptionMessage);
+    }
+
+    return ($targetFilePath);
+}
+?>
